799a436a2a8e5886ba17d5d0c05ba561a4c4343e
MikeeOo <michal.olszewskki@gmail.com>
2023-12-21 17:32:31 +0100

    changed navbar style to sticky

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,21 @@
-<nav class="navbar navbar-glass navbar-floating max-w-screen-xl font-headings">
-    <div class="navbar-start">
-        <a href="/" class="btn btn-ghost text-2xl navbar-item">
-            <span class="text-secondary font-bold ">DEV</span>JOBS
-        </a>
+<nav class="navbar navbar-glass navbar-sticky font-headings">
+    <div class="w-full max-w-screen-xl m-auto flex">
+        <div class="navbar-start">
+            <a href="/" class="btn btn-ghost text-2xl navbar-item">
+                <span class="text-secondary font-bold ">DEV</span>JOBS
+            </a>
+        </div>
+        <ul class="navbar-end uppercase">
+            <li>
+                <a href="/" class="btn btn-solid-secondary navbar-item ">
+                    Log in
+                </a>
+            </li>
+            <li>
+                <a href="/" class="btn btn-outline-secondary navbar-item hover:!text-white">
+                    Sign up
+                </a>
+            </li>
+        </ul>
     </div>
-    <ul class="navbar-end uppercase">
-        <li>
-            <a href="/" class="btn btn-solid-secondary navbar-item ">
-                Log in
-            </a>
-        </li>
-        <li>
-            <a href="/" class="btn btn-outline-secondary navbar-item hover:!text-white">
-                Sign up
-            </a>
-        </li>
-    </ul>
 </nav>
