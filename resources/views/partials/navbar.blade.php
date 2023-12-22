c5b9c6fb3794314f4cb5e5c93e294883169c330a
MikeeOo <michal.olszewskki@gmail.com>
2023-12-22 20:23:21 +0100

    fixed login link visibility

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,13 @@
-<nav class="navbar navbar-glass navbar-sticky font-headings">
+<nav class="navbar navbar-glass navbar-sticky">
     <div class="w-full max-w-screen-xl m-auto flex">
         <div class="navbar-start">
             <a href="/" class="btn btn-ghost text-2xl navbar-item">
-                <span class="text-secondary font-bold ">DEV</span>JOBS
+                <span class="text-secondary font-bold">DEV</span>JOBS
             </a>
         </div>
         <ul class="navbar-end uppercase">
             <li>
-                <a href="/" class="btn btn-solid-secondary navbar-item ">
+                <a href="/" class="link navbar-item text-sm hover:underline">
                     Log in
                 </a>
             </li>
