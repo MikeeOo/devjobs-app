13cc1fb4395500c671ff006bbbfa38db6a91f4a4
MikeeOo <michal.olszewskki@gmail.com>
2023-12-22 11:56:04 +0100

    created search bar

---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,15 @@
+<form action="">
+    <div class="relative max-w-screen-md m-auto">
+        <div class="absolute top-4 left-5 text-content3">
+            <i class="fa fa-search"></i>
+        </div>
+        <label hidden for="search">Devjobs search</label>
+        <input type="text" name="search" id="search" placeholder="Search for devjobs..."
+                class="input input-secondary input-rounded input-block w-full pl-12 h-14"/>
+        <div class="absolute top-2 right-2">
+            <button type="submit">
+                <span class="btn btn-secondary btn-rounded">Search</span>
+            </button>
+        </div>
+    </div>
+</form>
