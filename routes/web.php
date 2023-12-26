4f88a59ed740b1963643b53c7ae8f49fcf14da24
MikeeOo <michal.olszewskki@gmail.com>
2023-12-26 17:14:41 +0100

    created single listing page

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('pages.register');
 });
+
+Route::get('/listing', function () {
+    return view('pages.listing');
+});
