850ffd3a98d5aa821f607af8af4b79a8165cf8f4
MikeeOo <michal.olszewskki@gmail.com>
2023-12-26 18:03:36 +0100

    fixed icons visibility in light mode footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="fixed bottom-0 left-0 font-bold h-[60px] navbar navbar-glass">
     <div class="w-11/12 max-w-screen-xl m-auto flex">
     <div class="navbar-start space-x-2">
-        <a href="/" class="btn btn-outline-secondary btn-xs btn-circle text-base text-white"><i class="fa-brands fa-github"></i></a>
-        <a href="/" class="btn btn-outline-secondary btn-xs btn-circle text-base text-white"><i class="fa-brands fa-linkedin-in"></i></a>
+        <a href="/" class="btn btn-outline-secondary btn-xs btn-circle text-base text-content1"><i class="fa-brands fa-github"></i></a>
+        <a href="/" class="btn btn-outline-secondary btn-xs btn-circle text-base text-content1"><i class="fa-brands fa-linkedin-in"></i></a>
     </div>
 
     <div class="navbar-center">
