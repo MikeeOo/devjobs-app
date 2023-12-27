cc505a7302d50cd48d07ecf0ec93c499e1768836
MikeeOo <michal.olszewskki@gmail.com>
2023-12-27 09:24:43 +0100

    enabled vite hot refresh

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -33,5 +33,6 @@
         };
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/rippleui@1.12.1/dist/css/styles.css"/>
+    @vite('resources/js/app.js')
     <title>devjobs-app</title>
 </head>
