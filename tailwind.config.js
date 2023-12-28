4dc09dc2e1c540a76aabc4ebf6f2957c2604e54f
MikeeOo <michal.olszewskki@gmail.com>
2023-12-28 15:26:59 +0100

    installed prettier with blade and tailwind extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 import rippleui from "rippleui";
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-            main: ["Noto Sans", "sans-serif"],
-            headings: ["Space Grotesk", "sans-serif"],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ["Noto Sans", "sans-serif"],
+                headings: ["Space Grotesk", "sans-serif"],
+            },
         },
     },
-  },
-    plugins: [rippleui],
-}
-
+    plugins: [rippleui, "prettier-plugin-tailwindcss"],
+};
