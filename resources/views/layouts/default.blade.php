67e300d9bbc58ac542d210ba7c3c8355e6d5825c
MikeeOo <michal.olszewskki@gmail.com>
2023-12-28 19:15:31 +0100

    refactored layouts default using prettier

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,14 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en" data-theme="dark">
-{{--<html lang="en" data-theme="light" style="color-scheme: dark;" class=" ">--}}
-@include('partials.head')
-<body class="font-headings">
-@include('partials.navbar')
-<main class="mt-[60px]">
-@yield('content')
-</main>
-<div class="h-[60px]">
-@include('partials.footer')
-</div>
-</body>
+    {{-- <html lang="en" data-theme="light" style="color-scheme: dark;" class=" "> --}}
+    @include("partials.head")
+    <body class="font-headings">
+        @include("partials.navbar")
+        <main class="mt-[60px]">
+            @yield("content")
+        </main>
+        <div class="h-[60px]">
+            @include("partials.footer")
+        </div>
+    </body>
 </html>
