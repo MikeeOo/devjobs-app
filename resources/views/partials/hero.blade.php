57ebebfa6f9d63c42f19812bd8e936c239f169f6
MikeeOo <michal.olszewskki@gmail.com>
2023-12-29 19:22:33 +0100

    refactored partials hero using prettier

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,10 +1,11 @@
-<section class="flex flex-col justify-center align-center text-center py-12 w-11/12 mx-auto">
+<section class="align-center mx-auto flex w-11/12 flex-col justify-center py-12 text-center">
     <h1 class="text-6xl uppercase">
-        <span class="text-secondary">FIND </span>A NEW JOB...
+        <span class="text-secondary">FIND</span>
+        A NEW JOB...
     </h1>
-    <p class="text-2xl text-content2 my-4">or something...</p>
+    <p class="my-4 text-2xl text-content2">or something...</p>
     <div>
-        <a href="/" class="btn btn-outline-secondary navbar-item uppercase hover:!text-white px-6">
+        <a href="/" class="btn btn-outline-secondary navbar-item px-6 uppercase hover:!text-white">
             Sign Up to List a Gig
         </a>
     </div>
