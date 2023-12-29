30500fbe9e546e65616fe4ef2fd1f05fc758447c
MikeeOo <michal.olszewskki@gmail.com>
2023-12-29 19:23:22 +0100

    refactored partials search using prettier

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,9 +3,14 @@
         <div class="absolute top-4 left-5 text-content3">
             <i class="fa fa-search"></i>
         </div>
-        <label hidden for="search">Devjobs search</label>
-        <input type="text" name="search" id="search" placeholder="Search for devjobs..."
-                class="input input-secondary input-rounded input-block w-full pl-12 h-14"/>
+        <label for="search" hidden>Devjobs search</label>
+        <input
+            id="search"
+            name="search"
+            type="text"
+            placeholder="Search for devjobs..."
+            class="input input-secondary input-rounded input-block w-full pl-12 h-14"
+        />
         <div class="absolute top-2 right-2">
             <button type="submit">
                 <span class="btn btn-secondary btn-rounded">Search</span>
