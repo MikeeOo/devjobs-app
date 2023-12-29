a17992d413d5700732770e1e558d7b14c37cae02
MikeeOo <michal.olszewskki@gmail.com>
2023-12-29 19:21:23 +0100

    refactored partials navbar using prettier

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,22 +2,17 @@
     <div class="w-full max-w-screen-xl m-auto flex">
         <div class="navbar-start">
             <a href="/" class="btn btn-ghost text-2xl navbar-item">
-                <span class="text-secondary font-bold">DEV</span>JOBS
+                <span class="text-secondary font-bold">DEV</span>
+                JOBS
             </a>
         </div>
         <ul class="navbar-end uppercase">
             <li>
-                <a href="/" class="link navbar-item text-sm text-content1 link-underline-hover">
-                    Log in
-                </a>
+                <a href="/" class="link navbar-item text-sm text-content1 link-underline-hover">Log in</a>
             </li>
             <li>
-                <a href="/" class="btn btn-outline-secondary navbar-item hover:!text-white">
-                    Sign up
-                </a>
+                <a href="/" class="btn btn-outline-secondary navbar-item hover:!text-white">Sign up</a>
             </li>
         </ul>
     </div>
 </nav>
-
-
