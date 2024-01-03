3f6aaf58aa69351a4ecc2c698a55d3663dfc79ae
MikeeOo <michal.olszewskki@gmail.com>
2024-01-03 10:48:34 +0100

    implemented auth methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,5 +4,23 @@ namespace App\Http\Controllers;
 
 class AuthController extends Controller
 {
-    //
+    public function registerView()
+    {
+    }
+
+    public function register()
+    {
+    }
+
+    public function loginView()
+    {
+    }
+
+    public function login()
+    {
+    }
+
+    public function logout()
+    {
+    }
 }
