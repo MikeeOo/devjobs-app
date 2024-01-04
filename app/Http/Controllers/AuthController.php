07209c74b24188b144a9259ef98887544326ec5f
MikeeOo <michal.olszewskki@gmail.com>
2024-01-04 11:24:23 +0100

    returned auth views form AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+
 class AuthController extends Controller
 {
-    public function registerView()
+    public function registerView(): View
     {
+        return view('auth.register');
     }
 
     public function register()
     {
     }
 
-    public function loginView()
+    public function loginView(): View
     {
+        return view('auth.login');
     }
 
     public function login()
