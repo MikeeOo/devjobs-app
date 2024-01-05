6f4df475b79bf129607a3b3a256f9a56f05c1673
MikeeOo <michal.olszewskki@gmail.com>
2024-01-05 16:34:04 +0100

    added surname and login fields to register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,24 +8,51 @@
             <h2 class="text-2xl font-semibold mb-2">Sign Up</h2>
             <p class="text-sm">Create an account to post gigs</p>
         </div>
-
         <form method="post" action="{{ route('register') }}">
             @csrf
             <div class="form-group">
                 <div class="form-field relative pb-2">
-                    <label for="name" class="form-label">Full Name</label>
+                    <label for="name" class="form-label">Name</label>
                     <input
                         id="name"
                         name="name"
                         type="text"
-                        placeholder="e.g., Bonnie Green"
+                        placeholder="e.g., Jane"
                         class="input input-solid input-rounded max-w-full border border-content3 focus:border-secondary focus:placeholder-[#9750DD]"
                     />
                     <label class="form-label absolute bottom-0 left-0 translate-y-2/3">
                         <span class="form-label-alt text-error font-bold">Please enter a valid name.</span>
                     </label>
                 </div>
-
+                <div class="form-field relative pb-2">
+                    <label for="surname" class="form-label">Surname</label>
+                    <input
+                        id="surname"
+                        name="surname"
+                        type="text"
+                        placeholder="e.g., Doe"
+                        class="input input-solid input-rounded max-w-full border border-content3 focus:border-secondary focus:placeholder-[#9750DD]"
+                    />
+                    <label class="form-label absolute bottom-0 left-0 translate-y-2/3">
+                        <span class="form-label-alt text-error font-bold">Please enter a valid surname.</span>
+                    </label>
+                </div>
+                <div class="form-field relative pb-2">
+                    <label for="login" class="form-label">
+                        Login
+                        <span class="text-content3">(optional)</span>
+                    </label>
+                    <input
+                        id="login"
+                        name="login"
+                        type="text"
+                        placeholder="e.g., CodeWarrior777"
+                        class="input input-solid input-rounded max-w-full border border-content3 focus:border-secondary focus:placeholder-[#9750DD]"
+                    />
+                    <label class="form-label absolute bottom-0 left-0 translate-y-2/3">
+                        <span class="form-label-alt text-error font-bold">Please enter a valid login.</span>
+                    </label>
+                </div>
                 <div class="form-field relative pb-2">
                     <label for="email" class="form-label">Your email</label>
                     <input
@@ -39,7 +66,6 @@
                         <span class="form-label-alt text-error font-bold">Please enter a valid email.</span>
                     </label>
                 </div>
-
                 <div class="form-field relative pb-2">
                     <label for="password" class="form-label">Password</label>
                     <div class="form-control">
@@ -55,13 +81,12 @@
                         <span class="form-label-alt text-error font-bold">Invalid password.</span>
                     </label>
                 </div>
-
                 <div class="form-field relative pb-2">
-                    <label for="password" class="form-label">Confirm password</label>
+                    <label for="password_repeat" class="form-label">Confirm password</label>
                     <div class="form-control">
                         <input
-                            id="password"
-                            name="password"
+                            id="password_repeat"
+                            name="password_repeat"
                             type="password"
                             placeholder="Repeat your password"
                             class="input input-rounded input-solid max-w-full border border-content3 focus:border-secondary focus:placeholder-[#9750DD]"
@@ -71,7 +96,6 @@
                         <span class="form-label-alt text-error font-bold">Passwords do not match.</span>
                     </label>
                 </div>
-
                 <div class="form-field pt-5">
                     <div class="form-control">
                         <button type="submit" class="w-24 mx-auto">
@@ -79,11 +103,8 @@
                         </button>
                     </div>
                 </div>
-
-                <div class="">
-                    <div class="form-control justify-center">
-                        <a class="link text-content3 text-sm link-underline-hover">Already have an account? Log in.</a>
-                    </div>
+                <div class="form-control justify-center">
+                    <a class="link text-content3 text-sm link-underline-hover">Already have an account? Log in.</a>
                 </div>
             </div>
         </form>
