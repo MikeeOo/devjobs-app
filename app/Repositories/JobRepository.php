2a69bb87f066093ce66d1373317ddcec3a246204
MikeeOo <michal.olszewskki@gmail.com>
2024-01-13 11:10:01 +0100

    created Repositories Interface and implmented it in JobRepository

--- app/Repositories/JobRepository.php
+++ app/Repositories/JobRepository.php
@@ -2,6 +2,32 @@
 
 namespace App\Repositories;
 
-class JobRepository
+use App\Interfaces\IRepository;
+
+class JobRepository implements IRepository
 {
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function getById($id)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function create($data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update($id, $data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
 }
