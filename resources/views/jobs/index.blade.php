bb72d2164d88df2e6e7bfaaea930ae0c3dc897c2
MikeeOo <michal.olszewskki@gmail.com>
2024-01-19 18:28:19 +0100

    deleted hero file and moved it into jobs index

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-    @include('partials.hero')
+    <section class="align-center mx-auto flex w-11/12 flex-col justify-center py-12 text-center">
+        <h1 class="text-6xl uppercase">
+            <span class="text-secondary">FIND</span>
+            A NEW JOB...
+        </h1>
+        <p class="my-4 text-2xl text-content2">or something...</p>
+        <div>
+            @guest
+                <a
+                    href="{{ route('register.view') }}"
+                    class="btn btn-outline-secondary navbar-item px-6 uppercase hover:!text-white"
+                >
+                    Sign Up to List a Gig
+                </a>
+            @endguest
+
+            @auth
+                <a
+                    href="{{ route('jobs.create') }}"
+                    class="btn btn-outline-secondary navbar-item px-6 uppercase hover:!text-white"
+                >
+                    Create a Gig
+                </a>
+            @endauth
+        </div>
+    </section>
     @include('partials.search')
     <div class="w-11/12 mx-auto mt-12 max-w-screen-xl">
         <div class="lg:grid lg:grid-cols-2 lg:gap-4 mb-4">
