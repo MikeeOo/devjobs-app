e11f0a3c0ffdc14e86671474327f4550017ea51b
MikeeOo <michal.olszewskki@gmail.com>
2024-01-24 17:09:56 +0100

    created Image repository

---
+++ app/Repositories/ImageRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\IRepository;
+
+class ImageRepository implements IRepository
+{
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function getById($id)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function create($data)
+    {
+        //        Image::create($data);
+    }
+
+    public function update($job, $data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($job)
+    {
+        // TODO: Implement delete() method.
+    }
+}
