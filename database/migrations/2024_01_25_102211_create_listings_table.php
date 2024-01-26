57853fad8dd804c836040c849926330049796c23
MikeeOo <michal.olszewskki@gmail.com>
2024-01-26 12:42:27 +0100

    altered migration to reflect listing input order

--- database/migrations/2024_01_25_102211_create_listings_table.php
+++ database/migrations/2024_01_25_102211_create_listings_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title');
-            $table->string('logo')->nullable();
-            $table->string('tags');
             $table->string('company');
+            $table->string('title');
             $table->string('location');
             $table->string('email');
             $table->string('website');
+            $table->string('tags');
+            $table->string('logo')->nullable();
             $table->longText('description');
             $table->timestamps();
         });
