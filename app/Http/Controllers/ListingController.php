e4d5bcabe4f773554413e7698055597be07b6471
MikeeOo <michal.olszewskki@gmail.com>
2024-01-29 08:43:14 +0100

    initialized listing repository calls in ListingController

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,20 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateListingRequest;
 use App\Http\Requests\UpdateListingRequest;
 use App\Models\Listing;
+use App\Repositories\ListingRepository;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class ListingController extends Controller
 {
+    private ListingRepository $listingRepository;
+
     public function __construct()
     {
+        $this->listingRepository = new ListingRepository();
     }
 
     public function index(): View
     {
-        $jobs = $this->jobRepository->getAll(8, 1);
+        $listings = $this->listingRepository->getAll(8, 1);
 
-        return view('jobs.index', compact('jobs'));
+        return view('jobs.index', compact('listings'));
     }
 
     public function create(): View
@@ -28,7 +32,7 @@ class ListingController extends Controller
 
     public function store(CreateListingRequest $request): RedirectResponse
     {
-        $this->jobRepository->create($request->all());
+        $this->listingRepository->create($request->all());
 
         return redirect(route('index'))->with('success', 'Listing created successfully!');
     }
@@ -40,26 +44,26 @@ class ListingController extends Controller
 
     public function manage(): View
     {
-        $jobs = $this->jobRepository->getAll(8, 1);
+        $listings = $this->listingRepository->getAll(8, 1);
 
-        return view('jobs.manage', compact('jobs'));
+        return view('jobs.manage', compact('listings'));
     }
 
-    public function edit(Listing $job): View
+    public function edit(Listing $listing): View
     {
-        return view('jobs.edit', compact('job'));
+        return view('jobs.edit', compact('listing'));
     }
 
     public function update(UpdateListingRequest $request, Listing $listing): RedirectResponse
     {
-        $this->jobRepository->update($listing, $request->only('title', 'body'));
+        $this->listingRepository->update($listing, $request->all());
 
         return redirect(route('index'))->with('success', 'Listing updated successfully!');
     }
 
     public function delete(Listing $listing): RedirectResponse
     {
-        $this->jobRepository->delete($listing);
+        $this->listingRepository->delete($listing);
 
         return redirect(route('index'))->with('success', 'Listing DESTROYED BUHAHAHA!!!');
     }
