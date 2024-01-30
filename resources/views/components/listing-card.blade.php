0cc42dd6db733375292986452dba7c7d993d25a5
MikeeOo <michal.olszewskki@gmail.com>
2024-01-30 11:02:18 +0100

    changed job variable to listing variable in listing-card component

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,19 +1,19 @@
 @props([
-    'job',
+    'listing',
 ])
 <div class="bg-backgroundSecondary rounded-lg mb-4 lg:mb-0">
     <div class="flex">
         <img
-            src="{{ url('storage/images/' . $job->logo->url) }}"
+            src="{{ url('storage/images/' . $listing->logo) }}"
             alt=""
             class="hidden md:block rounded-l-lg w-48 h-48"
         />
         <div class="flex justify-center items-center md:ml-6 md:py-0 pr-3 ml-8 py-4">
             <div>
                 <h3 class="text-2xl link-underline-hover hover:text-content2">
-                    <a href="{{ route('jobs.show', $job->id) }}">{{ $job->title }}</a>
+                    <a href="{{ route('jobs.show', $listing->id) }}">{{ $listing->title }}</a>
                 </h3>
-                <div class="text-xl font-bold mb-4">{{ $job->company->name }}</div>
+                <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
                 <ul class="flex">
                     <li class="btn btn-solid-secondary btn-rounded px-3 mr-2 btn-xs">
                         <a href="#">Node.js</a>
