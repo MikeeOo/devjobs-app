18e9ddb14b8abe066fe6bf0687a8e6d24430b276
MikeeOo <michal.olszewskki@gmail.com>
2024-02-05 10:18:47 +0100

    created listing-tags component

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -1,1 +1,14 @@
-<?php
+@props([
+    'tagsCsv',
+])
+@php
+    $tags = explode(',', $tagsCsv);
+@endphp
+
+<ul class="flex">
+    @foreach ($tags as $tag)
+        <li class="btn btn-solid-secondary btn-rounded px-3 mr-2 btn-xs">
+            <a href="#">{{ $tag }}</a>
+        </li>
+    @endforeach
+</ul>
