467736f7b389e6aa4cd6ec56a1e583edfdb58307
MikeeOo <michal.olszewskki@gmail.com>
2024-02-05 19:00:25 +0100

    added tag param link to tags component

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -8,7 +8,7 @@
 <ul class="flex">
     @foreach ($tags as $tag)
         <li class="btn btn-solid-secondary btn-rounded px-3 mr-2 btn-xs">
-            <a href="#">{{ $tag }}</a>
+            <a href="/?tag={{ $tag }}">{{ $tag }}</a>
         </li>
     @endforeach
 </ul>
