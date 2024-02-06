b5abc12aecc248abb706ffa301d12072454eefbb
MikeeOo <michal.olszewskki@gmail.com>
2024-02-06 07:53:31 +0100

    merged update and create listing request into one ListingRequest

--- app/Http/Requests/ListingRequest.php
+++ app/Http/Requests/ListingRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateListingRequest extends FormRequest
+class ListingRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
