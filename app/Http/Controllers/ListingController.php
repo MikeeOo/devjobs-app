ef405c93b60552419a1265230140a4b94f7637a5
MikeeOo <michal.olszewskki@gmail.com>
2024-02-21 12:13:35 +0100

    changed create to store method name in repository

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -31,7 +31,7 @@ class ListingController extends Controller
 
     public function store(ListingRequest $request): RedirectResponse
     {
-        $this->listingRepository->create($request);
+        $this->listingRepository->store($request);
 
         return redirect(route('index'))->with('success', 'Listing created successfully!');
     }
